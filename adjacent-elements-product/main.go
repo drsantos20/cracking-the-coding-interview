ff30199cf1fc7ce9709da26a8edafb18f4a0d18b
Daniel Santos <drsantos20@gmail.com>
2018-07-13 18:33:36 -0300

    another solution

--- adjacent-elements-product/main.go
+++ adjacent-elements-product/main.go
@@ -2,27 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 
-	var highest_number float64
+	inputArray := []int{3, 6, -2, -5, 7, 3}
 
-	a := []int{3, 6, -2, -5, 7, 3}
-	max := len(a) - 1
-
-	for i := 0; i < max; i++ {
-		if i != max {
-			highest_number = math.Abs(float64(a[i])) * math.Abs(float64(a[i+1]))
-
-			if highest_number < math.Abs(float64(a[i]))*math.Abs(float64(a[i+1])) {
-				highest_number = math.Abs(float64(a[i])) * math.Abs(float64(a[i+1]))
-			}
-
+	max := -1001
+	for i := 0; i < len(inputArray)-1; i++ {
+		prod := inputArray[i] * inputArray[i+1]
+		if prod > max {
+			max = prod
 		}
-
 	}
-	fmt.Println(highest_number)
+	fmt.Println(inputArray)
 
 }
